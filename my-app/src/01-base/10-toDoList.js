c4f9032ea53b5b0ea61a3ef7e3a6e1c3c5b94bd6
ww520eeeee <ww520eeee@gmail.com>
2023-02-28 23:48:02 +0900

    20230228 React学习

---
+++ my-app/src/01-base/10-toDoList.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import './css/01-index.css'
+
+export default class App extends Component {
+
+  myRef = React.createRef()
+  state = {
+    list: ['aa', 'bb', 'cc']
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>10-toDoList</h1>
+        <input ref={this.myRef} />
+        <button onClick={this.handleClick2}>add2</button>
+        <ul>
+          {
+            this.state.list.map((item, index) =>
+              <li key={index}>{item}
+                {/* <button onClick={this.handleDelClick.bind(this, index)}>del</button> */}
+
+                <button onClick={() => { this.handleDelClick(index) }}>del</button>
+              </li>)
+          }
+        </ul>
+        {this.state.list.length === 0 ? <div>暂无代办事项</div> : null}
+        {/* 前面为真的话，后面就执行。 */}
+        {this.state.list.length === 0 && <div>暂无代办事项</div>}
+        {/* <div>
+            暂无代办事项
+          </div> */}
+
+        <div className={this.state.list.length===0 ? '':'hidden'}>暂无代办事项</div>
+      </div >
+    )
+  }
+
+  handleClick2 = () => {
+    console.log('click2', this.myRef.current.value)
+    // 最好不要修改原状态值
+    // this.state.list.push(this.myRef.current.value)
+    let newList = [...this.state.list]
+    newList.push(this.myRef.current.value)
+    console.log(newList)
+    this.setState({
+      list: newList
+    })
+    console.log(this.state.list)
+    // 清空输入框
+    this.myRef.current.value = ''
+  }
+
+  handleClick3 = () => {
+    console.log(this.state.list)
+  }
+
+  handleDelClick = (index) => {
+    console.log('进行删除' + index)
+    // concat()
+    let newList = this.state.list.slice()
+    newList.splice(index, 1)
+    this.setState({
+      list: newList
+    })
+  }
+}
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@
 import React from 'react'
 // import ReactDOM from 'react-dom'
 import { createRoot } from 'react-dom/client';
-import App from "./01-base/09-循环渲染"
+import App from "./01-base/10-toDoList"
 
 // React18的写法
 const container = document.getElementById('root');
